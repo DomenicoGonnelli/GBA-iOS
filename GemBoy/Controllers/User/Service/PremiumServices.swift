20fe3c5dd6a95ea7f4a51f6a073a01e4212ce66a
Domenico Gonnelli <domenico.gonnelli@outlook.it>
2025-08-28 12:15:42 +0200

    fix on premium service

--- GemBoy/Controllers/User/Service/PremiumServices.swift
+++ GemBoy/Controllers/User/Service/PremiumServices.swift
@@ -12,7 +12,7 @@ class PremiumServices {
     static func getAllPremium(_ completion: @escaping ([PremiumSubscriptionModel])->Void){
         
         //"https://www.dropbox.com/scl/fi/ezdml6l77dgla5kfcu1m6/premiumConfig.json?rlkey=3rpsoqnb4u235tjefpywdpkf2&dl=1"
-        let url = "https://www.dropbox.com/scl/fi/kc5sw9iub7m1wnubv6jy3/premiumCollaboration.json?rlkey=v3m5smo4uktsloy9xo8rhf9jz&dl=1"
+        let url = "https://firebasestorage.googleapis.com/v0/b/gemboyadvance-d0331.firebasestorage.app/o/service%2FpremiumCollaboration.json?alt=media&token=af163232-25b9-4f88-94bb-b5433ba480da"
         ServiceHelper.instance.driveService(url: url, request: nil, method: .get){ response in
             var list = [PremiumSubscriptionModel]()
             if let items = response?["premiumObject"] as? [Dictionary<String,Any>] {
